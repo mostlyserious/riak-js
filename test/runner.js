dfc829b1eb3aa66fc4a448e115eeb732cdd29ced
Francisco Treacy <francisco.treacy@gmail.com>
2011-11-19 18:32:30 +0100

    improve runner

--- test/runner.js
+++ test/runner.js
@@ -1,5 +1,6 @@
 var seq = require('seq'),
   fs = require('fs'),
+  util = require('util'),
   spawn = require('child_process').spawn,
   files = fs.readdirSync(__dirname);
   
@@ -14,8 +15,25 @@ files = files
 
 seq(files)
   .seqEach(function(file) {
-    // spawn ...
-    
-    console.log(file)
-    this.ok()
+    console.log("=> " + file);
+    exec(file, this);
+  })
+  .seq(function() {
+    console.log('[\033[32m' + 'SUCCESS' + '\033[39m]');
   });
+  
+
+
+function exec(test, callback) {
+  
+  var child = spawn('node', [test]);
+  child.stdout.pipe(process.stdout, { end: false });
+  child.stderr.pipe(process.stderr, { end: false });
+  
+  child.on('exit', function(code) {
+    console.log();
+    if (code == 0) return callback(null);
+    console.error('[\033[31m' + 'FAILURE' + '\033[39m] ' + test + " returned with code " + code);
+  });
+  
+}
