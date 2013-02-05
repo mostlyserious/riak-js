80c0f376dbee91e0e5485bf4a2611a9274d1086a
Mathias Meyer <meyer@paperplanes.de>
2013-02-05 20:37:31 +0100

    Adapt test to use should/mocha.

--- test/no-travis/http-client-pool-test.js
+++ test/no-travis/http-client-pool-test.js
@@ -1,47 +1,43 @@
 var HttpClient = require('../../lib/http-client'),
   HttpMeta = require('../../lib/http-meta'),
-  seq = require('seq'),
+  should = require('should'),
   util = require('util'),
   assert = require('assert'),
   test = require('../../lib/utils').test;
 
-var db = new HttpClient({ pool: {
-  servers: [
-    'localhost:10018', 
-    'localhost:10028', 
-    'localhost:10038', 
-    'localhost:10048'
-  ],
-  options: {}
-}});
+var db;
 
-seq()
-.seq(function() {
-  test('Creates an object');
-  db.save('languages', 'erlang', {type: 'functional'}, function(err) {
-    this.ok();
-  }.bind(this));
-})
-.seq(function() {
-  test('Get that object');
-  db.get('languages', 'erlang', function(err, data) {
-    assert(!err);
-    assert.equal(data.type, 'functional');
-    this.ok();
-  }.bind(this));
-})
-.seq(function() {
-  test('Get the streamed object');
-  var self = this;
-  db.get('languages', 'erlang', {stream: true}, function(err, response, meta) {
-    assert(!err);
-    response.on('data', function(data) {
-      data = JSON.parse(String(data));
-      assert.equal(data.type, 'functional');
-      self.ok();
-    }.bind(this));
+describe('http-client-pool', function () {
+  before(function(done) {
+    db = new HttpClient({ pool: {
+      servers: [
+        'localhost:8098',
+        'localhost:8098'
+      ],
+      options: {}
+    }});
+    done();
   });
+
+  it('Creates an object', function(done) {
+    db.save('languages', 'erlang', {type: 'functional'}, function(err) {
+      db.get('languages', 'erlang', function(err, data) {
+        should.not.exist(err);
+        data.type.should.equal('functional');
+        done();
+      });
+    });
+  });
+
+  it('Fetches the streamed object', function(done) {
+    db.get('languages', 'erlang', {stream: true}, function(err, response, meta) {
+      assert(!err);
+      response.on('data', function(data) {
+        data = JSON.parse(String(data));
+        data.type.should.equal('functional');
+        done();
+      });
+    });
+  })
 })
-.seq(function() {
-  process.exit(0);
-});
+
