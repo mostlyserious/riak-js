c6902191c718f075145dd8ff8d40048ca92b326a
Mathias Meyer <meyer@paperplanes.de>
2013-12-15 16:37:02 +0100

    Add matrix entry for Riak 2.0.0pre5.

---
+++ install_riak.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if $RIAK_VERSION != "default"
+then
+  echo "Stopping Riak service"
+  sudo service riak stop
+  sudo apt-get remove riak
+  riak_file="riak_$RIAK_VERSION-1_amd64.deb"
+  cd /tmp
+  echo "Downloading Riak $RIAK_VERSION"
+  wget http://s3.amazonaws.com/downloads.basho.com/riak/$RIAK_RELEASE/$RIAK_VERSION/ubuntu/precise/$riak_file
+  echo "Installing Riak $RIAK_VERSION"
+  sudo dpkg -i $riak_file
+  echo "Starting Riak..."
+  sudo service riak start
+fi
