064b9e03e4432c74d0253fa9bacf103900f4e4b4
Alexander Popov <apopov@meta-comm.com>
2014-11-24 14:47:56 +0300

    fix install riak

--- install_riak.sh
+++ install_riak.sh
@@ -8,7 +8,7 @@ then
   riak_file="riak_$RIAK_VERSION-1_amd64.deb"
   cd /tmp
   echo "Downloading Riak $RIAK_VERSION"
-  echo http://s3.amazonaws.com/downloads.basho.com/riak/$RIAK_RELEASE/$RIAK_VERSION/ubuntu/precise/$riak_file
+  wget http://s3.amazonaws.com/downloads.basho.com/riak/$RIAK_RELEASE/$RIAK_VERSION/ubuntu/precise/$riak_file
   echo "Installing Riak $RIAK_VERSION"
   sudo dpkg -i $riak_file
   echo "Enabling Yokozuna"
